2c09042d6bc02fb7835483e194e31a20745be1b8
Only-bottle <bmlee@nota.ai>
2023-08-07 00:55:40 +0900

    ADD: 20.5 Github Log In

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -12,7 +12,12 @@ export default function SocialLogin() {
         <Divider />
       </HStack>
       <VStack>
-        <Button w="100%" leftIcon={<FaGithub />}>
+        <Button
+          as="a"
+          href="https://github.com/login/oauth/authorize?client_id=c39ea00b6a5010f68c2b&scope=read:user,user:email"
+          w="100%"
+          leftIcon={<FaGithub />}
+        >
           Continue with Github
         </Button>
         <Button w="100%" leftIcon={<FaComment />} colorScheme={"yellow"}>
