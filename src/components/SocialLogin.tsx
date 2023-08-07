719160a24f11c93f4b154b6729b0e968d82dffbf
Only-bottle <bmlee@nota.ai>
2023-08-07 23:24:59 +0900

    ADD: 20.10 Kakao Talk Auth

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -2,6 +2,17 @@ import { FaComment, FaGithub } from "react-icons/fa";
 import { Box, Button, Divider, HStack, Text, VStack } from "@chakra-ui/react";
 
 export default function SocialLogin() {
+  const githubParams = {
+    client_id: "c39ea00b6a5010f68c2b",
+    scope: "read:user,user:email",
+  };
+  const github_params = new URLSearchParams(githubParams).toString();
+  const kakaoParams = {
+    client_id: "33564d2c5ea25acf55caf57c7b334bf4",
+    redirect_uri: "http://127.0.0.1:3000/social/kakao",
+    response_type: "code",
+  };
+  const kakao_params = new URLSearchParams(kakaoParams).toString();
   return (
     <Box mb={4}>
       <HStack my={8}>
@@ -14,13 +25,19 @@ export default function SocialLogin() {
       <VStack>
         <Button
           as="a"
-          href="https://github.com/login/oauth/authorize?client_id=c39ea00b6a5010f68c2b&scope=read:user,user:email"
+          href={`https://github.com/login/oauth/authorize?${github_params}`}
           w="100%"
           leftIcon={<FaGithub />}
         >
           Continue with Github
         </Button>
-        <Button w="100%" leftIcon={<FaComment />} colorScheme={"yellow"}>
+        <Button
+          as="a"
+          href={`https://kauth.kakao.com/oauth/authorize?${kakao_params}`}
+          w="100%"
+          leftIcon={<FaComment />}
+          colorScheme={"yellow"}
+        >
           Continue with Kakao
         </Button>
       </VStack>
